79bd55ac69b182a51e02265aea417d0d176eaf35
Daivik <114691318+User-DK@users.noreply.github.com>
2023-10-04 22:48:51 +0530

    Add files via upload

---
+++ python/hello_world.py
@@ -0,0 +1,4 @@
+print("Hello, World!") 
+
+
+#Python is a high-level, dynamically typed (meaning that Python infers the type of a variable based on the value assigned to it.), and versatile programming language known for its simplicity and readability.
